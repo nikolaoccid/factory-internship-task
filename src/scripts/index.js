0ac5ed017fff9e22ada8cc22bd6c7a186adb523e
Nikola Occidentale <nikolaoccid@gmail.com>
2023-03-07 23:19:44 +0100

    Code cleanup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,6 @@
 import '../styles/index.scss';
 import $ from 'jquery';
 
-function extendSlider(selector){
-  const viewportWidth = window.innerWidth;
-  const textareaWidth = $('.textarea').width();
-  let sliderDivWidth = $(`.${selector}`).width();
-  let leftScreenSpace = viewportWidth - textareaWidth - sliderDivWidth;
-
-  if (leftScreenSpace > 0) {
-    const childElements = $(`.${selector}`).children().clone();
-    $(`.${selector}`).append(childElements);
-    extendSlider(selector);
-  }
-}
 function disableButtons(){
   $('.left-button').prop('disabled', true);
   $('.right-button').prop('disabled', true);
@@ -73,8 +61,6 @@ function slideRight(){
   // enableButtons() function called after the animation;
 }
 $(document).ready(() => {
-  // extendSlider('top-slider');
-  // extendSlider('bottom-slider');
   $('.left-button').on('click', slideLeft);
   $('.right-button').on('click', slideRight);
 
